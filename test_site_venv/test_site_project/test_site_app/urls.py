dc9490cab2ceed056bd22ede4d73538bd5af72c1
git-junk <77789860+git-junk@users.noreply.github.com>
2021-11-23 05:34:34 +0900

    コミットだよ。

--- test_site_venv/test_site_project/test_site_app/urls.py
+++ test_site_venv/test_site_project/test_site_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "test_site_app"
 
 urlpatterns = [
-    path("sample/", views.index, name="index"),
+    path("index/", views.index, name="index"),
 ]
--- test_site_venv/test_site_project/test_site_project/urls.py
+++ test_site_venv/test_site_project/test_site_project/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("index/", include("test_site_app.urls")),
+    path("", include("test_site_app.urls")),
     path('admin/', admin.site.urls),
 ]
